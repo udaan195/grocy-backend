2a10c75b988824af2d87ad230a8da05c79bacc7d
udaan195 <pranshukumar9519@gmail.com>
2025-07-07 08:56:08 +0530

    Update authMiddleware.js

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -3,25 +3,16 @@ const User = require('../api/models/userModel');
 
 const protect = async (req, res, next) => {
     let token;
-
     if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
         try {
-            // 'Bearer' शब्द हटाकर टोकन निकालें
             token = req.headers.authorization.split(' ')[1];
-            
-            // टोकन को वेरिफाई करें
             const decoded = jwt.verify(token, process.env.JWT_SECRET);
-
-            // यूजर की जानकारी निकालें (बिना पासवर्ड के)
             req.user = await User.findById(decoded.id).select('-password');
-            
-            next(); // अगले मिडिलवेयर या रूट पर जाएँ
+            next();
         } catch (error) {
-            console.error(error);
             res.status(401).json({ message: 'Not authorized, token failed' });
         }
     }
-
     if (!token) {
         res.status(401).json({ message: 'Not authorized, no token' });
     }
